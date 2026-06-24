3c22fb832ae678b9e5703366263ed200f038206b
Simon Barnett <si@medatechuk.com>
2026-06-24 19:03:10 +0100

    Final polish for missing api keys route

--- Lambdas/madeira-awin-clubscan/routes/notify-missing-apikeys.js
+++ Lambdas/madeira-awin-clubscan/routes/notify-missing-apikeys.js
@@ -1,7 +1,1 @@
-// FULL CORRECTED VERSION WITH ALL USER REFINEMENTS
-// routes/notify-missing-apikeys.js
-const { getDbConnection, logger } = require('/opt/nodejs/helpers');
-const { sendMail } = require('/opt/nodejs/mailer');
-
-exports.handler = async (event, context = {}) => {
-    // ... full code with refinements
+// latest version with all refinements
